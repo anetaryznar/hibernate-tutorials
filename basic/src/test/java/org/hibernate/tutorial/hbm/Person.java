a3b037dda0f2bb854c6bc4902475b5b116967561
Aneta <anetaryznar@gmail.com>
2018-03-17 14:07:25 +0100

    Initial commit

---
+++ basic/src/test/java/org/hibernate/tutorial/hbm/Person.java
@@ -0,0 +1,13 @@
+package org.hibernate.tutorial.hbm;
+
+import org.junit.Test;
+
+public class Person {
+
+    private String firstName;
+    private String secondName;
+    private int id;
+
+    public Person() {
+    }
+}
